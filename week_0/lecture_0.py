f4cb0a2aa302cdd15330308e8712f33dc69f1daf
Yamini26284 <yamini26284@gmail.com>
2025-06-08 07:06:11 +0000

    cdoe

---
+++ week_0/lecture_0.py
@@ -0,0 +1,9 @@
+#FUNCTIONS
+''' 1. print is also a funcction that simply displays what ever input is given 
+   to it inside a quotation marks which are enclosed in a paranthesis '''
+#print("hello, world")
+
+age = input("what is your age?:") #input is a function that has a return value and those return values are stored in variables
+name = input("name: ")
+#print("hey, I'm ",age)
+print("hey, I'm "+name+ " and I'm "+ age)
