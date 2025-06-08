6a76ac7a74520a5c78f8896ea0d494e060457306
Yamini26284 <yamini26284@gmail.com>
2025-06-08 10:12:11 +0000

    do

--- week_0/lecture_0.py
+++ week_0/lecture_0.py
@@ -5,5 +5,7 @@
 
 age = input("what is your age?:") #input is a function that has a return value and those return values are stored in variables
 name = input("name: ")
-#print("hey, I'm ",age)
-print("hey, I'm "+name+ " and I'm "+ age)
+print("hey, I'm ",age) #function with multipe argumenats 
+print("hey, I'm",age)
+#print("hey, I'm "+name+ " and I'm "+ age)
+# this is a single argument which uses concatenation.
